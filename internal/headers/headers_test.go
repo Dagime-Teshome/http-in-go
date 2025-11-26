b7cd5ca9ae61969588afa4352018faa1dc8646c1
Dagime Teshome <dagimeteshome1@gmail.com>
2025-11-26 20:53:07 +0300

    add header parser package

---
+++ internal/headers/headers_test.go
@@ -0,0 +1,33 @@
+package headers
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestHeadersParsing(t *testing.T) {
+	// Test: Valid single header
+
+	h := NewHeaders()
+	data := []byte("Host: localhost:42069\r\n\r\n")
+	n, done, err := h.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, h)
+	assert.Equal(t, "localhost:42069", h["Host"])
+	assert.Equal(t, 23, n)
+	assert.False(t, done)
+
+	// Test: Invalid spacing header
+	h = NewHeaders()
+	data = []byte("       Host : localhost:42069       \r\n\r\n")
+	n, done, err = h.Parse(data)
+	require.Error(t, err)
+	assert.Equal(t, 0, n)
+	assert.False(t, done)
+}
+
+func NewHeaders() Headers {
+	return Headers{}
+}
