fc100d2b99d7e4a2cd4f19cb09d39749b0e95174
Dagime Teshome <dagimeteshome1@gmail.com>
2025-11-27 19:36:03 +0300

    add addtional header test

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -26,6 +26,19 @@ func TestHeadersParsing(t *testing.T) {
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
+
+	// "Valid single header with extra whitespace"
+
+	h = NewHeaders()
+	data = []byte("Host:localhost:42069\r\nAuthKey:sfs4392\r\n\r\n")
+	n, done, err = h.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, h)
+	assert.Equal(t, "localhost:42069", h["Host"])
+	assert.Equal(t, "sfs4392", h["AuthKey"])
+	assert.Equal(t, 39, n)
+	assert.False(t, done)
+
 }
 
 func NewHeaders() Headers {
