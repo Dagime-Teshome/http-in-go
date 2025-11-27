9fc3549e2823b2d559df6d5b7fa8d8e488ec6f9c
Dagime Teshome <dagimeteshome1@gmail.com>
2025-11-27 20:11:11 +0300

    Remove header parse logic from request module

--- internal/request/request.go
+++ internal/request/request.go
@@ -30,33 +30,17 @@ type RequestLine struct {
 
 func (r *Request) parse(data []byte) (int, error) {
 	var bytesParsed int
-	requestDone := false
 	switch r.Status {
 	case initialized:
-		if requestDone {
-			// call header parse
-			header, byteParsed, err := parseHeader(data)
-			if err != nil {
-				return 0, err
-			}
-			if byteParsed == 0 {
-				return 0, err
-			}
-			if len(header) == 1 {
-				r.Status = done
-			}
-			r.Headers[header[0]] = header[1]
-		} else {
-			requestLine, bytesParsed, err := parseRequestLine(data)
-			if err != nil {
-				return 0, errors.New("Error parsing stream")
-			}
-			if bytesParsed == 0 {
-				return 0, nil
-			}
-			r.RequestLine = *requestLine
-			requestDone = true
+
+		requestLine, bytesParsed, err := parseRequestLine(data)
+		if err != nil {
+			return 0, errors.New("Error parsing stream")
 		}
+		if bytesParsed == 0 {
+			return 0, nil
+		}
+		r.RequestLine = *requestLine
 
 	case done:
 		return 0, errors.New("Trying to read from done parser")
@@ -121,33 +105,6 @@ func parseRequestLine(request []byte) (*RequestLine, int, error) {
 	// the +2 is to take into account the \r\n characters
 	bytesParsed := crlfIndex + 2
 	return requestLine, bytesParsed, nil
-
-}
-
-func parseHeader(request []byte) ([]string, int, error) {
-	headerEnd := bytes.Index(request, []byte(headerEnd))
-	if headerEnd == -1 {
-		crlfIndex := bytes.Index(request, []byte(crlf))
-		if crlfIndex == -1 {
-			return nil, 0, nil
-		}
-		headerText := string(request[:crlfIndex])
-		headerSlice, err := getHeaderFromString(headerText)
-		if err != nil {
-			return nil, 0, err
-		}
-		return headerSlice, crlfIndex + 2, nil
-
-	}
-	header := []string{" "}
-	return header, 0, nil
-}
-func getHeaderFromString(s string) ([]string, error) {
-	headerParts := strings.Split(s, ":")
-	if len(headerParts) != 2 {
-		return nil, errors.New("invalid header length")
-	}
-	return headerParts, nil
 
 }
 
