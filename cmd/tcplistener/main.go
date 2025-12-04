ac81c0c91e747e3d117dd6d3b3d7932933ec3213
Dagime Teshome <dagimeteshome1@gmail.com>
2025-12-04 22:30:53 +0300

    refactor request parser code

--- cmd/tcplistener/main.go
+++ cmd/tcplistener/main.go
@@ -49,28 +49,70 @@ func getLinesChannel(file io.ReadCloser) <-chan string {
 	return c
 }
 
-func main() {
+// func main() {
 
-	listener, err := net.Listen("tcp", ":42069")
+// 	listener, err := net.Listen("tcp", ":42069")
+// 	if err != nil {
+// 		log.Fatalf("error listening for TCP traffic: %s\n", err.Error())
+// 	}
+// 	defer listener.Close()
+
+// 	for {
+// 		connection, err := listener.Accept()
+// 		if err != nil {
+// 			log.Fatalf("error: %s\n", err.Error())
+// 		}
+// 		// fmt.Println("Accepted Connection From", connection.RemoteAddr())
+// 		// line := getLinesChannel(connection)
+// 		request, err := request.RequestFromReader(connection)
+
+// 		if err != nil {
+// 			log.Fatal(err)
+// 		}
+// 		// for line := range line {
+// 		// 	fmt.Println(line)
+// 		// }
+// 		// fmt.Println("Connection to ", connection.RemoteAddr(), "closed")
+
+// 		fmt.Printf("Request line:\n -Method:%s\n -Target:%s\n -Version:%s\n -Headers:\n", request.RequestLine.Method, request.RequestLine.RequestTarget, request.RequestLine.HttpVersion)
+
+// 		for key, value := range request.Headers {
+// 			fmt.Printf("-%s:%s", key, value)
+// 		}
+
+// 		fmt.Printf("Request line:\n - Method: %s \n - Target: %s \n - Version: %s", request.RequestLine.Method, request.RequestLine.RequestTarget, request.RequestLine.HttpVersion)
+
+// 	}
+// }
+
+const port = ":42069"
+
+func main() {
+	listener, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("error listening for TCP traffic: %s\n", err.Error())
 	}
 	defer listener.Close()
 
+	fmt.Println("Listening for TCP traffic on", port)
 	for {
-		connection, err := listener.Accept()
+		conn, err := listener.Accept()
 		if err != nil {
 			log.Fatalf("error: %s\n", err.Error())
 		}
-		// fmt.Println("Accepted Connection From", connection.RemoteAddr())
-		// line := getLinesChannel(connection)
-		request, err := request.RequestFromReader(connection)
-		// for line := range line {
-		// 	fmt.Println(line)
-		// }
-		// fmt.Println("Connection to ", connection.RemoteAddr(), "closed")
+		fmt.Println("Accepted connection from", conn.RemoteAddr())
 
-		fmt.Printf("Request line:\n - Method: %s \n - Target: %s \n - Version: %s", request.RequestLine.Method, request.RequestLine.RequestTarget, request.RequestLine.HttpVersion)
-
+		req, err := request.RequestFromReader(conn)
+		if err != nil {
+			log.Fatalf("error parsing request: %s\n", err.Error())
+		}
+		fmt.Println("Request line:")
+		fmt.Printf("- Method: %s\n", req.RequestLine.Method)
+		fmt.Printf("- Target: %s\n", req.RequestLine.RequestTarget)
+		fmt.Printf("- Version: %s\n", req.RequestLine.HttpVersion)
+		fmt.Println("Headers:")
+		for key, value := range req.Headers {
+			fmt.Printf("- %s: %s\n", key, value)
+		}
 	}
 }
