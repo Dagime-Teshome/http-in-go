505c700219342fb29d626197999afba6454bbbfa
Dagime Teshome <dagimeteshome1@gmail.com>
2025-12-16 20:29:14 +0300

    add video type handler

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"MODULE_NAME/internal/headers"
 	"MODULE_NAME/internal/request"
 	"MODULE_NAME/internal/response"
 	"MODULE_NAME/internal/server"
+	"crypto/sha256"
 	"fmt"
 	"io"
 	"log"
@@ -40,16 +42,37 @@ func handler(w *response.Writer, req *request.Request) {
 		return
 	}
 	if strings.HasPrefix(req.RequestLine.RequestTarget, "/httpbin") {
-		response, err := sendHttpRequest(req.RequestLine.RequestTarget)
-		if err != nil {
-			handler500(w, req)
-		}
-		chunkedHandler(w, response.Body)
+		proxyHandler(w, req)
+		return
+		// response, err := sendHttpRequest(req.RequestLine.RequestTarget)
+		// if err != nil {
+		// 	handler500(w, req)
+		// }
+		// chunkedHandler(w, response.Body)
+		// return
+	}
+	if strings.HasPrefix(req.RequestLine.RequestTarget, "/video") {
+		videoHandler(w, req)
 		return
 	}
 }
 
+func videoHandler(w *response.Writer, req *request.Request) {
+	w.WriteStatusLine(response.StatusOK)
+	vidBytes, err := os.ReadFile("assets/vim.mp4")
+	if err != nil {
+		log.Fatal(err)
+		handler500(w, req)
+	}
+	headers := response.GetDefaultHeaders(len(vidBytes))
+	headers.SetOVR("Content-Type", "video/mp4")
+	w.WriteHeaders(headers)
+	w.WriteBody(vidBytes)
+
+}
+
 func chunkedHandler(w *response.Writer, p io.ReadCloser) {
+	defer p.Close()
 	buffer := make([]byte, 32)
 	headers := response.GetDefaultHeaders(0)
 	w.WriteStatusLine(200)
@@ -58,16 +81,17 @@ func chunkedHandler(w *response.Writer, p io.ReadCloser) {
 	w.WriteHeaders(headers)
 	for {
 		n, err := p.Read(buffer)
-		if n == 0 {
-			w.WriteChunkedBodyDone()
-		}
 		if err != nil {
 			if err == io.EOF {
+				w.WriteChunkedBodyDone()
 				break
 			}
 			handler500(w, &request.Request{})
+			return
 		}
-		w.WriteChunkedBody(buffer[:n])
+		if n > 0 {
+			w.WriteChunkedBody(buffer[:n])
+		}
 	}
 }
 
@@ -91,7 +115,7 @@ func handler400(w *response.Writer, _ *request.Request) {
 }
 
 func handler500(w *response.Writer, _ *request.Request) {
-	w.WriteStatusLine(response.StatusBadRequest)
+	w.WriteStatusLine(response.StatusInternalError)
 	body := []byte(`<html>
 		<head>
 		<title>500 Internal Server Error</title>
@@ -133,4 +157,60 @@ func sendHttpRequest(target string) (*http.Response, error) {
 		return nil, err
 	}
 	return response, nil
+}
+
+func proxyHandler(w *response.Writer, req *request.Request) {
+	target := strings.TrimPrefix(req.RequestLine.RequestTarget, "/httpbin/")
+	url := "https://httpbin.org/" + target
+	fmt.Println("Proxying to", url)
+	resp, err := http.Get(url)
+	if err != nil {
+		handler500(w, req)
+		return
+	}
+	defer resp.Body.Close()
+
+	w.WriteStatusLine(response.StatusOK)
+	h := response.GetDefaultHeaders(0)
+	h.SetOVR("Transfer-Encoding", "chunked")
+	h.SetOVR("Trailer", " X-Content-Sha256, X-Content-Length")
+	h.Delete("Content-Length")
+	w.WriteHeaders(h)
+
+	const maxChunkSize = 1024
+	buffer := make([]byte, maxChunkSize)
+	var hashBuffer []byte
+	for {
+		n, err := resp.Body.Read(buffer)
+		fmt.Println("Read", n, "bytes")
+		if n > 0 {
+			hashBuffer = append(hashBuffer, buffer[:n]...)
+			_, err = w.WriteChunkedBody(buffer[:n])
+			if err != nil {
+				fmt.Println("Error writing chunked body:", err)
+				break
+			}
+		}
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Println("Error reading response body:", err)
+			break
+		}
+	}
+	_, err = w.WriteChunkedBodyDone()
+	trailer := addTrailer(hashBuffer)
+	w.WriteTrailers(trailer)
+	if err != nil {
+		fmt.Println("Error writing chunked body done:", err)
+	}
+}
+
+func addTrailer(buf []byte) headers.Headers {
+	sum := sha256.Sum256(buf)
+	headers := headers.NewHeaders()
+	headers.Set("X-Content-Sha256", fmt.Sprintf("%x", sum))
+	headers.Set("X-Content-Length", fmt.Sprintf("%d", len(buf)))
+	return headers
 }
